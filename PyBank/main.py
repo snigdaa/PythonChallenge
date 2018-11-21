ab6dbde1c3beafdc218b05bdf74676ee2aae7553
Sairathi <snigdaa.ram@gmail.com>
2018-11-21 18:35:06 -0500

    finished PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,2 +1,47 @@
 #main file for PyBank
+import os
+import csv
 
+
+filename = "budget_data.csv"
+monthlist = []
+proflosslist = []
+totMoney = 0
+changeList = []
+
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader,None)
+    for row in csvreader:
+        monthlist.append(row[0])
+        proflosslist.append(int(row[1]))
+        totMoney += int(row[1])
+        
+numMonths = len(monthlist)
+
+for x in range(1,(len(monthlist)-1)):
+    change = proflosslist[x] - proflosslist[x-1]
+    changeList.append(change)
+
+highest = 0
+lowest = 0
+totchanges = 0
+highestmonth = ""
+lowestmonth = ""
+for idx, changes in enumerate(changeList):
+    if changes > highest:
+        highest = changes
+        highestmonth = monthlist[idx+1]
+    if changes < lowest:
+        lowest = changes
+        lowestmonth = monthlist[idx+1]
+    totchanges += changes
+
+avgChange = round((totchanges/len(changeList)),2)
+
+print("Financial Analysis \n--------------------------")
+print(f"Total Months: {numMonths}")
+print(f"Total: {totMoney}")
+print(f"Average Change: ${avgChange}")
+print(f"Greatest Increase in Profits: {highestmonth} (${highest})")
+print(f"Greatest Decrease in Profits: {lowestmonth} (${lowest})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,73 @@
 #main file for PyPoll
+import os
+import csv
+
+filename = "election_data.csv"
+
+numVotes = 0
+candidates = []
+index=""
+
+match = False
+
+#open csv and compile all candidate votes into a list "candidates"
+#also tally how many votes were submitted with an iterator numVotes
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader,None)
+    for row in csvreader:
+        numVotes+=1
+        candidates.append(row[2])
+
+#add the first candidate to the unique candidates list
+uniqueCand = []
+uniqueCand.append(candidates[0])
+
+#sort only unique candidates into the "uniqueCand" list
+for candidate in candidates:
+    for each in uniqueCand:
+        if each == candidate:
+            match=True
+            break
+    if match==True:
+        match=False
+        continue
+    else:
+        uniqueCand.append(candidate)
+
+#calculate how many votes each one got
+voterList=[]
+for each in uniqueCand:
+    voterList.append(0)
+
+for idx,each in enumerate(uniqueCand):
+    for candidate in candidates:
+        if candidate==each:
+            voterList[idx]+=1
+
+#Calculate each candidate's percent won + number of votes won
+percentList = []
+
+for votes in voterList:
+    percent = round(((votes/numVotes)*100),2)
+    percentList.append(percent)
+
+#Find out who's the winner
+highest = 0
+for idx,votes in enumerate(voterList):
+    if int(votes) > highest:
+        highest = int(votes)
+        index = idx
+winner = uniqueCand[index]
+
+#winner: winner
+#Percent won list: percentList
+#Total votes: numVotes
+#List of each candidate's votes: voterList
+#candidates: uniqueCand
+
+print(numVotes)
+print(uniqueCand)
+print(voterList)
+print(percentList)
+print(winner)
